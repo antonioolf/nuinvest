19d6e46a1117ada5ea22494d27be3e4b8d4fc1a5
Antônio Oliveira <antonio.filho128@gmail.com>
2022-05-04 11:19:40 -0300

    Bugfix

--- background.js
+++ background.js
@@ -7,34 +7,43 @@ chrome.action.onClicked.addListener((tab) => {
   }
 });
 
-function runScript() {
+async function runScript() {
     const token = localStorage.getItem('access_token');
 
-    fetch('https://www.nuinvest.com.br/api/samwise/v2/custody-position', {
+    const response = await fetch('https://www.nuinvest.com.br/api/samwise/v2/custody-position', {
         method: 'GET',
         headers: {
             'authorization': `Bearer ${token}`
         }
-    })
-    .then((response) => response.json())
-    .then((data) => {
-        const cards = document.querySelectorAll('[data-testid="financial-card"]');
-        [...cards].map((card, i) => {
-            const investment = data.investments[i];
-
-            const lucroPrejuizo = (investment.lastPrice - investment.averagePrice) * investment.quantity;
-            const gainLossClass = lucroPrejuizo > 0 ? 'gain' : 'loss';
-            
-            const newInfo = document.createElement('div');
-            newInfo.innerHTML = `
-                <div class="new-info ${gainLossClass}">
-                    <div>Lucro/Prejuízo: ${format(lucroPrejuizo)}</div>
-                </div>
-            `
-            card.appendChild(newInfo);
-        });
     });
 
+    const data = await response.json();
+    const cards = document.querySelectorAll('[data-testid="financial-card"]');
+    [...cards].map(async (card, i) => {
+        const investment = data.investments[i];
+
+        const response = await fetch(`https://www.nuinvest.com.br/api/gringott/averagePrice/${investment.stockCode}`, {
+            method: 'GET',
+            headers: {
+                'authorization': `Bearer ${token}`
+            }
+        });
+
+        const averagePrice = await response.json();
+        console.log(averagePrice);
+
+        const lucroPrejuizo = (investment.lastPrice - averagePrice.averagePrice) * investment.quantity;
+        const gainLossClass = lucroPrejuizo > 0 ? 'gain' : 'loss';
+        
+        const newInfo = document.createElement('div');
+        newInfo.innerHTML = `
+            <div class="new-info ${gainLossClass}">
+                <div>Lucro/Prejuízo: ${format(lucroPrejuizo)}</div>
+            </div>
+        `
+        card.appendChild(newInfo);
+    });
+    
     const format = (value) => {
         return value.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
     }
